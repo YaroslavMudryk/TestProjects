6be9c4efa3fa28c7b6eb86cd5f58072dc7f59ff5
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2024-01-16 22:18:18 +0200

    Add project for UniqId

---
+++ TestProjects/TestProjects.UniqIds/Generator.cs
@@ -0,0 +1,123 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TestProjects.UniqIds
+{
+    public static class Generator
+    {
+        private static long counter = 0;
+        private static object lockObject = new object();
+
+        public static string GetV1()
+        {
+            long num = DateTime.Now.Ticks, nbase = 36;
+            String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            if (nbase < 2 || nbase > chars.Length)
+                return "";
+
+            long r;
+            String newNumber = "";
+
+            while (num >= nbase)
+            {
+                r = num % nbase;
+                newNumber = chars[(int)r] + newNumber;
+                num = num / nbase;
+            }
+
+            newNumber = chars[(int)num] + newNumber;
+
+            return newNumber.ToLower();
+        }
+
+        public static string GetV2()
+        {
+            return DateTime.Now.Ticks.ToString().GetHashCode().ToString("x");
+        }
+
+        public static string GetV3()
+        {
+            Random rand = new Random((int)DateTime.Now.Ticks);
+            int random = rand.Next(1, 100000000);
+
+            return random.ToString();
+        }
+
+        public static string GetV4()
+        {
+            return DateTime.Now.ToString("yymmddhhmmssfff");
+        }
+
+        public static string GetV5()
+        {
+            long timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            long uniqueId = (int)(timestamp % int.MaxValue) + counter;
+            counter++;
+
+            return uniqueId.ToString();
+        }
+
+        public static string GetV6()
+        {
+            lock (lockObject)
+            {
+                long timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+                long combinedValue = (timestamp << 20) | (counter++ % (1 << 20));
+
+                char[] CharacterSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
+                int Base = CharacterSet.Length;
+
+                string result = string.Empty;
+
+                do
+                {
+                    result = CharacterSet[combinedValue % Base] + result;
+                    combinedValue /= Base;
+                } while (combinedValue > 0);
+
+                return result;
+            }
+        }
+
+        public static string GetV7()
+        {
+            Guid guid = Guid.NewGuid();
+
+            string base64String = Convert.ToBase64String(guid.ToByteArray());
+
+            string shortId = base64String
+                .Replace("/", "_")
+                .Replace("+", "-")
+                .TrimEnd('=');
+
+            return shortId;
+        }
+
+        public static string GetV8()
+        {
+            Guid uniqueValue = Guid.NewGuid();
+
+            string combinedData = uniqueValue.ToString("N") + DateTime.UtcNow.Ticks;
+
+            byte[] hashBytes;
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(combinedData));
+            }
+
+            string shortId = BitConverter.ToString(hashBytes, 0, 4).Replace("-", "").ToLower();
+
+            return shortId;
+        }
+
+        public static string GetV9()
+        {
+            var ticks = DateTime.Now.Ticks;
+            var bytes = BitConverter.GetBytes(ticks);
+            var id = Convert.ToBase64String(bytes);
+            return id.Replace('+', '_').Replace('/', '-').TrimEnd('=');
+        }
+    }
+}
---
+++ TestProjects/TestProjects.UniqIds/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TestProjects.UniqIds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 100000;
+            CheckUniq(Generator.GetV1, count);
+            CheckUniq(Generator.GetV2, count);
+            CheckUniq(Generator.GetV3, count);
+            CheckUniq(Generator.GetV4, count);
+            CheckUniq(Generator.GetV5, count);
+            CheckUniq(Generator.GetV6, count);
+            CheckUniq(Generator.GetV7, count);
+            CheckUniq(Generator.GetV8, count);
+            CheckUniq(Generator.GetV9, count);
+        }
+
+        public static void CheckUniq(Func<string> action, int count)
+        {
+            Dictionary<string, int> uniq = new Dictionary<string, int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var id = action();
+
+                if (uniq.ContainsKey(id))
+                {
+                    uniq[id]++;
+                }
+                else
+                {
+                    uniq.Add(id, 1);
+                }
+            }
+
+            var repeatability = uniq.Where(s => s.Value > 1).OrderByDescending(s => s.Value).ToList();
+
+            Console.WriteLine($"Frequency is {(repeatability.Count * 100) / uniq.Count}%, repeated: {string.Join(',', repeatability.Select(s=> $"{s.Key}:{s.Value}"))}");
+        }
+    }
+}
