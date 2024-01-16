7ca88a1eaddce389975f96c82bacc577708d14d4
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2024-01-16 21:38:37 +0200

    Add project for Hangfire

---
+++ TestProjects/TestProjects.Hangfire/Program.cs
@@ -0,0 +1,46 @@
+using Hangfire;
+using TestProjects.Hangfire.Services;
+
+namespace TestProjects.Hangfire
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseInMemoryStorage());
+
+            builder.Services.AddScoped<OrderService>();
+
+            builder.Services.AddHangfireServer();
+
+
+            var app = builder.Build();
+
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ TestProjects/TestProjects.Hangfire/Services/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using Hangfire;
+
+namespace TestProjects.Hangfire.Services
+{
+    public class OrderService
+    {
+        private readonly IBackgroundJobClient _backgroundJobClient;
+        public OrderService(IBackgroundJobClient backgroundJobClient)
+        {
+            _backgroundJobClient = backgroundJobClient;
+        }
+
+        public void ProcessOrder()
+        {
+            _backgroundJobClient.Schedule(() => UpdateOrderStatus(), TimeSpan.FromHours(24)); // method will be executed in 24 hours
+        }
+
+        public void UpdateOrderStatus()
+        {
+            Console.WriteLine($"Статус оновлений");
+        }
+    }
+}
